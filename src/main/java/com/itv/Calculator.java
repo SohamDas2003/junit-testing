1a1425e6dfb87aad6cad74741d561fc7cd95ba7e
SohamDas2003 <sohamdas704@gmail.com>
2024-08-14 08:04:48 +0530

    initial commit

---
+++ src/main/java/com/itv/Calculator.java
@@ -0,0 +1,17 @@
+package com.itv;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+	public String hello() {
+		return "Hello";
+	}
+	
+	public boolean isAvailable() {
+		return false;
+	}
+	
+}
---
+++ src/test/java/com/itv/CalculatorTest.java
@@ -0,0 +1,45 @@
+package com.itv;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Before
+	public void before() {
+		System.out.println("Testing");
+	}
+	
+	@Test
+	public void testAdd1() {
+		Calculator cal = new Calculator();
+		int res = cal.add(2, 2);
+		assertEquals(4, res);
+	}
+	
+	@Test
+	public void testAdd2() {
+		Calculator cal = new Calculator();
+		int res = cal.add(4, 2);
+		assertEquals(6, res);
+	}
+
+	@Test
+	public void checkHelloNull() {
+		Calculator cal = new Calculator();
+		assertNull(cal.hello());
+	}
+	
+	@Test
+	public void checkBoolean() {
+		Calculator cal = new Calculator();
+		assertTrue(cal.isAvailable());
+	}
+	
+}
